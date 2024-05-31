0aaa618e07b9ad2e988006fdcea25c7f689cca77
Cliff L. Biffle <cliff@oxide.computer>
2024-05-31 09:46:02 -0700

    Hack Hubris support into rustaceanvim

--- nvim/lua/plugins/rustacean.lua
+++ nvim/lua/plugins/rustacean.lua
@@ -33,6 +33,103 @@ local rust_analyzer_settings = {
     },
 }
 
+local cache = {}
+local clients = {}
+local non_hubris_bufnrs = {}
+
+local cache_hubris_if_needed = function(client, bufnr)
+    -- Have we already cached this hubris info?
+    local cached = cache[bufnr]
+    if cached ~= nil then
+        return cached
+    end
+
+    -- Have we already checked this bufnr and decided it's not hubris?
+    if non_hubris_bufnrs[bufnr] ~= nil then
+        return nil
+    end
+
+    -- Might this be a hubris project?
+    local dir = client.config.root_dir
+    if not string.find(dir, "hubris") then
+        non_hubris_bufnrs[bufnr] = true
+        return nil
+    end
+
+    -- Run `xtask lsp` for the target file, which gives us a JSON
+    -- dictionary with bonus configuration.
+    local prev_cwd = vim.fn.getcwd()
+    vim.cmd("cd " .. dir)
+    local cmd = dir .. "/target/debug/xtask lsp "
+    -- Notify `xtask lsp` of existing clients in the CLI invocation,
+    -- so it can check against them first (which would mean a faster
+    -- attach)
+    for _,v in pairs(clients) do
+        local c = vim.fn.escape(vim.json.encode(v), '"')
+        cmd = cmd .. '-c"' .. c .. '" '
+    end
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+    local handle = io.popen(cmd .. bufname)
+    handle:flush()
+    local result = handle:read("*a")
+    handle:close()
+    vim.cmd("cd " .. prev_cwd)
+
+    -- If `xtask` doesn't know about `lsp`, then it will print an error to
+    -- stderr and return nothing on stdout.
+    if result == "" then
+        vim.notify("recompile `xtask` for `lsp` support", vim.log.levels.WARN)
+    end
+
+    -- If the given file should be handled with special care, then
+    -- we give the rust-analyzer client a custom name (to prevent
+    -- multiple buffers from attaching to it), then cache the JSON in
+    -- a local variable for use in `on_attach`
+    local json = vim.json.decode(result)
+    if json["Ok"] ~= nil then
+        -- Do rust-analyzer builds in a separate folder to avoid blocking
+        -- the main build with a file lock.
+        table.insert(json.Ok.buildOverrideCommand, "--target-dir")
+        table.insert(json.Ok.buildOverrideCommand, "target/rust-analyzer")
+        local cache_entry = {
+            name = "rust_analyzer_" .. json.Ok.hash,
+            json = json,
+        }
+        cache[bufnr] = cache_entry
+        table.insert(clients, { toml = json.Ok.app, task = json.Ok.task })
+        return cache_entry
+    else
+        non_hubris_bufnrs[bufnr] = true
+        return nil
+    end
+end
+
+local apply_hubris_config = function(client, json)
+    local config = vim.deepcopy(client.config)
+    local ra = config.settings["rust-analyzer"]
+    ra.cargo = {
+        extraEnv = json.Ok.extraEnv,
+        features = json.Ok.features,
+        noDefaultFeatures = true,
+        target = json.Ok.target,
+        buildScripts = {
+            overrideCommand = json.Ok.buildOverrideCommand,
+        },
+    }
+    ra.checkOnSave = {}
+    ra.check = {
+        overrideCommand = json.Ok.buildOverrideCommand,
+    }
+    config.lspinfo = function()
+        return {
+            "Hubris app:      " .. json.Ok.app,
+            "Hubris task:     " .. json.Ok.task,
+        }
+    end
+    client.config = config
+end
+
+
 return {
     "mrcjkb/rustaceanvim",
     version = "^4",
@@ -40,7 +137,12 @@ return {
     ft = { "rust" },
     opts = {
         server = {
-            on_attach = function(_, bufnr)
+            on_attach = function(client, bufnr)
+                local cached = cache_hubris_if_needed(client, bufnr)
+                if cached ~= nil then
+                    apply_hubris_config(client, cached.json)
+                end
+
                 -- TODO: I'm not actually sure why these are here.
                 vim.keymap.set("n", "<leader>cR", function()
                     vim.cmd.RustLsp("codeAction")
